adb508f7bfa9f8ea1d1fe24f1e86530a1e1e29a7
Xeonid <adrian-cybulski@outlook.com>
2015-06-20 12:41:55 +0100

    scope for eloquent, with reversing order by id

--- app/Mem.php
+++ app/Mem.php
@@ -8,7 +8,11 @@ class Mem extends Model
 {
     protected $table = "mems";
 
-
+    public function scopeReverseOrder($query)
+    {
+        return $query->orderBy('id','desc');
+    }
+    
     public function comments()
     {
     	return $this->hasMany('App\Comment');

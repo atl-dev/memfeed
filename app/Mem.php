924690e61f3ead052237de1ba5b2ca9e02ee286a
Xeonid <adrian-cybulski@outlook.com>
2015-06-20 16:32:38 +0100

    all methods / classes were documented

--- app/Mem.php
+++ app/Mem.php
@@ -8,20 +8,40 @@ use Input;
 class Mem extends Model
 {
     protected $table = 'mems';
-
+    /**
+     * Scope for reversed fetching data by id.
+     *
+     * @return Query object
+     *
+     * @param Query
+     **/
     public function scopeReverseOrder($query)
     {
         return $query->orderBy('id', 'desc');
     }
 
+    /**
+     * Creates relation.
+     *
+     * @return Relation
+     **/
     public function comments()
     {
         return $this->hasMany('App\Comment');
     }
+    /**
+     * Change status of mem to approved.
+     *
+     * @param Integer
+     **/
     public function approve($id)
     {
-    	\DB::update("UPDATE mems SET approved='yes' WHERE id = ?",[$id]);
+        \DB::update("UPDATE mems SET approved='yes' WHERE id = ?", [$id]);
     }
+
+    /**
+     * Uploads mem image file , and insert record whith mem details into a table.
+     **/
     public function addMem()
     {
         if (Input::hasFile('image')) {
@@ -35,7 +55,7 @@ class Mem extends Model
                 $ext = Input::file('image')->getClientOriginalExtension();
                 $name = md5($name).'.'.$ext;
                 Input::file('image')->move(__DIR__.'/../public/images/', $name);
-                \DB::insert('INSERT INTO mems (title,img_path) VALUES(?,?)', [ $data['title'],'/images/'.$name]);
+                \DB::insert('INSERT INTO mems (title,img_path) VALUES(?,?)', [$data['title'], '/images/'.$name]);
             }
         }
     }

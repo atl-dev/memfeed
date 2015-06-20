d480f0cbe0d61dafb675c73f865f6ab1238423d5
Xeonid <adrian-cybulski@outlook.com>
2015-06-20 14:02:42 +0100

    Added routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,3 +21,12 @@ Route::get('/view/mem/{id}', function ($id) {
 
     return view('mem')->with('mems', $mems);
 });
+
+Route::get('/manage/comments','AdminController@manageComments');
+Route::get('/manage/mems','AdminController@manageMems');
+Route::get('/approve/mem/{id}','AdminController@approveMem');
+Route::get('/approve/comment/{id}','AdminController@approveComment');
+
+Route::get('/add/mem',function(){ 
+	return view('add_mem_form');
+});

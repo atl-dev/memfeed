96c331780be84ba70dce7c9df773e20135593b1a
pumanitro <pumanitro@gmail.com>
2015-08-24 10:23:55 +0200

    Login form done.

--- resources/views/login_modal.php
+++ resources/views/login_modal.php
@@ -9,15 +9,19 @@
                 <!--                Miejsce użycia formy - admin.php-->
                 <form action="your_acc.php" method="post">
                     <div class="form-group" >
-                        <h5><label for="usr">Name:</label></h5>
+                        <h5><label for="usr"><i class="fa fa-user"></i> Name:</label></h5>
                         <input type="text" class="form-control" name="usr" id="usr">
                     </div>
                     <div class="form-group" >
-                        <h5><label for="pwd">Password:</label></h5>
+                        <h5><label for="pwd"><i class="fa fa-lock"></i> Password:</label></h5>
                         <input type="password" class="form-control" name="pwd" id="pwd">
                     </div>
                     <input style="width: 100%" type="submit" name="submit" value="Login" class="btn btn-default">
                 </form>
+            </div>
+            <div class="modal-footer">
+                <a href="registration.php">Need account?</a></br>
+                <a href="remember_psw.php">Forgot password?</a>
             </div>
         </div>
     </div>

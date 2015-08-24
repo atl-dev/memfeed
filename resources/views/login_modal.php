5c6664488aa5e7f876a0c06aec81ed5e9a9a0756
pumanitro <pumanitro@gmail.com>
2015-08-24 10:27:30 +0200

    Login form - DETAIL (exit button)

--- resources/views/login_modal.php
+++ resources/views/login_modal.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-sm">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <button type="button" class="close" data-dismiss="modal"><i style="color:white;" class="fa fa-times"></i></button>
                 <h4 class="modal-title">Log in form</h4>
             </div>
             <div class="modal-body">

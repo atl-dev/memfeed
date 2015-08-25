685dc296025889369a6917a77d086fcbcef72682
Xeonid <adrian-cybulski@outlook.com>
2015-08-25 14:31:52 +0100

    redirect to view profile

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -42,7 +42,8 @@ camelCase - ids in HTML  , variables in php ;
                 @if(!Auth::check())
                     <li><a  href="/auth/login" data-target="#loginModal" style="cursor: pointer;"><span class="glyphicon glyphicon-log-in"></span> Log In</a></li>
                 @else
-                    <li><a href="your_acc.php" style="cursor: pointer;"><i class="fa fa-user"></i> Your acount</a></li>
+
+                    <li><a href="/view/profile/{{Auth::user()->id}}" style="cursor: pointer;"><i class="fa fa-user"></i> Your acount</a></li>
                 @endif
             </ul>
         </div>

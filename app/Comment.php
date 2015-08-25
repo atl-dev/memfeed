e09734280b1d4ecbb7ef0d9a873a6d5f38962ba4
Xeonid <adrian-cybulski@outlook.com>
2015-08-25 13:45:05 +0100

    docs

--- app/Comment.php
+++ app/Comment.php
@@ -9,16 +9,35 @@ class Comment extends Model
     protected $table = "comments";
     protected $fillable = ['email','body','mem_id','user_id','approved'];
 
+
+    /**
+     * Relation to mem
+     *
+     * @return Query Object
+     *  
+     **/
     public function mem()
     {
     	return $this->belongsTo('App\Mem','mem_id');
     }
 
+
+    /**
+     * Relation to user
+     *
+     * @return Query Object 
+     **/
     public function author()
     {
         return $this->belongsTo('App\User','user_id');
     }
 
+    /**
+     * Approve comment
+     *
+     * @return void
+     * @param Int 
+     **/
     public function approve($id)
     {
     	$comment = App\Comment::findOrFail($id);

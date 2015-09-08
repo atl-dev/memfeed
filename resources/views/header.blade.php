70c6863c336adaeb86f75630e67194843dbe11f4
Xeonid <adrian-cybulski@outlook.com>
2015-09-08 20:12:36 +0100

    todo

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -41,7 +41,7 @@ camelCase - ids in HTML  , variables in php ;
                 </li>
                 @if(!Auth::check())
                     <!--@TODO use here your modal  -->
-                    <li><a  href="/auth/login" data-target="#loginModal" style="cursor: pointer;"><span class="glyphicon glyphicon-log-in"></span> Log In</a></li>
+                    <li><a data-target="#loginModal" style="cursor: pointer;"><span class="glyphicon glyphicon-log-in"></span> Log In</a></li>
                 @else
 
                     <li><a href="/view/profile/{{Auth::user()->id}}" style="cursor: pointer;"><i class="fa fa-user"></i> Your acount</a></li>

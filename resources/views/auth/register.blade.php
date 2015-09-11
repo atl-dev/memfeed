5321dc7b328d3bdca990949e60adb1471e8928d4
pumanitro <pumanitro@gmail.com>
2015-09-11 13:28:59 +0200

    Registration form done.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,36 +1,43 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
 @include('assets')
 </head>
 <body>
-<div class="container-fluid" style=" margin-top: 100px;">
+
 @include('partials.navbar.default')
 
-    <div class="row">
-        <div class="col-sm-2"></div>
+<div class="container-fluid"  id="footNav-container">
+    <div class="row" style=" margin-top: 74px;text-align: center;">
+        <div class="col-sm-3"></div>
         <div class='col-sm-6'>
-            <h4> Sign up</h4>
-            <form action="/auth/register" method="post">
-             {!! csrf_field() !!}
-                <div>
-                    <input type="text" name="name" class="form-control" placeholder="Name" value="{{ old('name') }}">
+            <div class="mem" style="padding-left: 10px;padding-right:10px;padding-bottom:10px;margin-top:10px;">
+                <div style="background-color: #222327;">
+                    <h3 style="margin-bottom:10px;margin-top:10px;padding-top:16px;padding-bottom:16px;"><i class="fa fa-user-plus"></i> Sign up</h3>
                 </div>
-                <div>
-                    <input type="email" placeholder="E-mail" class='form-control' name="email" value="{{ old('email') }}">
-                </div>
-                <div>
-                    <input type="password" placeholder="Password" class='form-control' name="password" id="password">
-                </div>
-                 <div>
-                    <input type="password" placeholder="Confirm password" class='form-control' name="password_confirmation" id="password">
-                </div>
-                <div>
-                    <button type="submit" class='btn btn-xs btn-primary nextPageBtn' >Sign up</button>
-                </div>
-            </form>
+                <form action="/auth/register" method="post">
+                 {!! csrf_field() !!}
+                    <div class="spaceLoginForm" style="padding-top:5px;">
+                        <input type="text" name="login" class="form-control input-lg" placeholder="Login" value="{{ old('name') }}">
+                    </div>
+                    <div class="spaceLoginForm">
+                        <input type="email" placeholder="E-mail" class='form-control input-lg' name="email" value="{{ old('email') }}">
+                    </div>
+                    <div class="spaceLoginForm">
+                        <input type="password" placeholder="Password" class='form-control input-lg' name="password" id="password">
+                    </div>
+                     <div class="spaceLoginForm">
+                        <input type="password" placeholder="Confirm password" class='form-control input-lg' name="password_confirmation" id="password">
+                    </div>
+                    <div class="spaceLoginForm" style="padding-top:5px;">
+                        <button type="submit" class='btn btn-lg btn-primary nextPageBtn' ><b>Register <i class="fa fa-arrow-circle-right"></i></b></button>
+                    </div>
+                </form>
+            </div>
         </div>
+        <div class="col-sm-3"></div>
     </div>
-
+    <div id="footer-push"></div>
 </div>
 @include('partials.page.footer')
 </body>
